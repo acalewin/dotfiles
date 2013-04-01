5e180e7fe8346e49c39ab4a2c7b42da98b209e08
Acalewin <acalewin@gmail.com>
2013-04-01 13:18:49 -0400

    Updated to catch and hopefully report connection errors. Also updated to properly get UTC time

--- wk_queue.py
+++ wk_queue.py
@@ -4,18 +4,23 @@
 import urllib
 import urllib.request
 import json
-import datetime
+import calendar
 import time;
 
-url='http://www.wanikani.com/api/user/<apikey>/study-queue/'
-resp = json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
+url='http://www.wanikani.com/api/user/<api_key>/study-queue/'
+try:
+	resp = json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
+except URLError:
+	print("URL ERR",end='')
+except Exception:
+	print("UNK ERR",end='')
 
 info = resp['requested_information']
 review_time = info['next_review_date']
-time_now = int(time.time())
+time_now = calendar.timegm(time.gmtime())
 if info['lessons_available'] > 0:
 	print("L:",info['lessons_available'],end=" - " )
-if time_now-review_time < 0:
-	print("R:",info['reviews_available'], end=' ')
+if info['reviews_available'] > 0:
+	print("R:",info['reviews_available'], end='')
 else:
-	print("R:",(time_now-review_time)/60, "mins", end=' ')
+	print("R:",int((review_time-time_now)/60), "mins", end='')
